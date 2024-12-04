d71b5147ea54d8eac35662a871ff17a002670442
haidg <haidg@vng.com.vn>
2024-12-04 11:01:12 +0700

    update spinner

--- app/components/custom/loading.component.tsx
+++ app/components/custom/loading.component.tsx
@@ -1,40 +1,24 @@
-export default function Loading() {
+export default function Loading({ size = 36 }: { size?: number }) {
   return (
-    <svg width="30" height="30" stroke="#000" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+    <svg width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <style>
         {`
         .spinner {
           transform-origin: center;
-          animation: spinner-rotate 2s linear infinite;
+          animation: spinner-rotate 0.75s infinite linear;
         }
-        .spinner circle {
-          stroke-linecap: round;
-          animation: spinner-dash 1.5s ease-in-out infinite;
-        }
+
         @keyframes spinner-rotate {
           100% {
             transform: rotate(360deg);
           }
         }
-        @keyframes spinner-dash {
-          0% {
-            stroke-dasharray: 0 150;
-            stroke-dashoffset: 0;
-          }
-          47.5% {
-            stroke-dasharray: 42 150;
-            stroke-dashoffset: -16;
-          }
-          95%, 100% {
-            stroke-dasharray: 42 150;
-            stroke-dashoffset: -59;
-          }
-        }
       `}
       </style>
-      <g className="spinner">
-        <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="3"></circle>
-      </g>
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner"
+      />
     </svg>
   );
 }

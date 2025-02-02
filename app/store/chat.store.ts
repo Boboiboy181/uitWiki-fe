c30e6e08fb4a9989ef6919cf29cb70d2e14ba6a1
haidg <haidg@vng.com.vn>
2025-02-02 11:01:16 +0700

    fix build

--- app/store/chat.store.ts
+++ app/store/chat.store.ts
@@ -1,6 +1,5 @@
-import { create, StateCreator } from 'zustand';
+import { create } from 'zustand';
 import { MessageType } from '~/types';
-import { logger } from './middleware';
 
 type ChatState = {
   messages: MessageType[];
@@ -12,16 +11,14 @@ type ChatState = {
   setIsError: (isError: boolean) => void;
 };
 
-const useChatStore = create<ChatState>()(
-  (process.env.NODE_ENV === 'development' ? logger : (fn: StateCreator<ChatState>) => fn)((set) => ({
-    messages: [],
-    setMessages: (messages: MessageType[]) => set({ messages }),
-    addMessage: (message: MessageType) => set((state) => ({ messages: [...state.messages, message] })),
-    isLoading: false,
-    isError: false,
-    setIsLoading: (isLoading: boolean) => set({ isLoading }),
-    setIsError: (isError: boolean) => set({ isError }),
-  })),
-);
+const useChatStore = create<ChatState>()((set) => ({
+  messages: [],
+  setMessages: (messages: MessageType[]) => set({ messages }),
+  addMessage: (message: MessageType) => set((state) => ({ messages: [...state.messages, message] })),
+  isLoading: false,
+  isError: false,
+  setIsLoading: (isLoading: boolean) => set({ isLoading }),
+  setIsError: (isError: boolean) => set({ isError }),
+}));
 
 export const useChat = () => useChatStore((state) => state);
